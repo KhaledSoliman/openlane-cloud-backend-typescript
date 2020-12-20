9feb9522277038606af929dc03beefeb0da7f6bc
Khaled Soliman <khaledsoli111@aucegypt.edu>
2020-12-20 15:15:41 +0200

    debugging openlane shell script

--- src/openlane-run.sh
+++ src/openlane-run.sh
@@ -30,7 +30,7 @@ while [ $# -gt 0 ]; do
   esac
   shift
 done
-PDK_ROOT=/home/ks/openlane-cloud/backend/openlane_working_dir/pdks
+PDK_ROOT=/home/ks/openlane-cloud-backend-typescript/openlane_working_dir/pdks
 
 cd openlane_working_dir/openlane || exit
 DOCKER_ID=$(sudo docker run --rm --cpus="$cpus" --memory="$memory" --name="$tag" -div  $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:cloud)
