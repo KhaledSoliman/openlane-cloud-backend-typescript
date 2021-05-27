7fa6278e1bb17e7b02082c3ebf929d4ed634b0b5
Khaled Soliman <khaledsoli111@aucegypt.edu>
2021-05-27 15:07:45 +0200

    debugging

--- src/slurm.sh
+++ src/slurm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sbatch --nodes=1 -t00:50:00 ./src/openlane-job.sh https://storage.googleapis.com/copper-array-312208-singularity gs://copper-array-312208-singularity-job-out "$@"
+sbatch --nodes=1 -t00:50:00 ./src/openlane-job.sh --cb=https://storage.googleapis.com/copper-array-312208-singularity --ob=gs://copper-array-312208-singularity-job-out "$@"
